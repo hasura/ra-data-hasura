5f58ab9a0d66ff945e127ae45711cace9692ed37
Bharat Kashyap <bharatkashyap@outlook.com>
2021-06-29 23:35:58 +0530

    add optional-chaining support inside babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,13 @@ module.exports = {
       {
         test: /\.js?$/,
         exclude: /(node_modules)/,
-        use: 'babel-loader',
+        use: {
+           loader: 'babel-loader',
+           options: {
+           presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        }
       },
       {
         test: /\.mjs$/,
