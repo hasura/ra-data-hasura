cc5f8e0e552321000e1320cad57f5702752c1354
Bharat Kashyap <bharatkashyap@outlook.com>
2021-06-29 23:40:20 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,12 @@ module.exports = {
         test: /\.js?$/,
         exclude: /(node_modules)/,
         use: {
-           loader: 'babel-loader',
-           options: {
-           presets: [
-            ['@babel/preset-env', { targets: "defaults" }]
-          ]
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
         }
       },
       {
