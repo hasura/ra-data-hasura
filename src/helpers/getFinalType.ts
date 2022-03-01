26cb0b600b6d7c37bc87ec3391b9c975fc308df6
Chris Vibert <cp.vibert@gmail.com>
2022-03-01 15:01:08 +0700

    Set up Jest and add helper tests

--- src/helpers/getFinalType.ts
+++ src/helpers/getFinalType.ts
@@ -14,10 +14,12 @@ type GraphQLTypes =
  * Ensure we get the real type even if the root type is NON_NULL or LIST
  * @param {GraphQLType} type
  */
-export const getFinalType = (type: GraphQLTypes): IntrospectionType => {
+const getFinalType = (type: GraphQLTypes): IntrospectionType => {
   if (type.kind === TypeKind.NON_NULL || type.kind === TypeKind.LIST) {
     return getFinalType(type.ofType);
   }
 
   return type as IntrospectionType;
 };
+
+export default getFinalType;
