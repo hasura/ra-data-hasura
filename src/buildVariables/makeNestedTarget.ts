64e69647a78f9ed632cd5591b3fe35213b776d7c
Maciej Kowalski <f.kowal@gmail.com>
2022-05-10 08:04:21 +0200

    document jsonb _contains usage, . -> # support same prefix standard

--- src/buildVariables/makeNestedTarget.ts
+++ src/buildVariables/makeNestedTarget.ts
@@ -16,14 +16,18 @@ export const makeNestedTarget = (
 ): NestedTarget<TargetEquals> => {
   // This simple example should make clear what this function does
   // makeNestedTarget("a.b", 42)
+  // makeNestedTarget("a#b", 42)
   // => { a: { b: { _eq: 42 } } }
-  // makeNestedTarget("a.b@_contains@c.d", id)
+  // makeNestedTarget("a#b@_contains@c#d", id)
   // => { a: { b: { _contains: { c: { d: 42 } } } } }
+  // . -> # to make nested filtering support the same separator/standard
 
   let [path, operation = '_eq', oppath] = target.split('@');
-  let value = oppath ? set({}, oppath.split('.'), id) : id;
+  let value = oppath ? set({}, oppath
+    .split(".").join("#") // nested filtering support the same standard
+    .split('#'), id) : id;
 
-  return set({}, path.split('.'), {
+  return set({}, path.split('.').join("#").split("#"), {
     [operation]: value,
   }) as NestedTarget<TargetEquals>;
 };
